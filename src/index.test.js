8fe7f544d129ad05f53ca1f79c45fdd1a3c0f6c4
Mehdi Khaleghian <Mehdi.Khaleghian@health.telstra.com>
2017-01-20 19:42:13 +1100

    fixed test logic

--- src/index.test.js
+++ src/index.test.js
@@ -8,8 +8,8 @@ describe('our first test',()=>{
     })
 });
 
-describe('index.html', (done)=>{
-    it('should say hello',()=>{
+describe('index.html', ()=>{
+    it('should say hello',(done)=>{
         const index = fs.readFileSync('./src/index.html',"utf-8");
         jsdom.env(index, function(err, window){
             const h1 = window.document.getElementsByTagName('h1')[0];
